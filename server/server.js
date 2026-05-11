05c876144adf98f580d607ef2324d9ce7f888591
Muhammadnur <valiyevmuhammadnur90@gmail.com>
2026-05-11 17:58:25 +0500

    mini fixes

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 import "dotenv/config";
 import express from "express";
+import { connectDB } from "./config/connectDB.js";
 
 const DB_URI = process.env.DB_URI;
 if (!DB_URI) {
   console.log("DB_URI not found from .env");
+  process.exit(1);
 }
 
 const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173"],
   }),
 );
 
