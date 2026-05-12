cbcc6a2e9fbab5609c8c8edf9357efcda5272dea
Muhammadnur <valiyevmuhammadnur90@gmail.com>
2026-05-12 17:22:53 +0500

    wide range of url

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://fullstackcrud-tan.vercel.app"],
   }),
 );
 
